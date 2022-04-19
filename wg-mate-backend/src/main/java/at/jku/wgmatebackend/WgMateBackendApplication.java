1c9d8b032c185872d76d95be9e03c428a1c1ce25
Dani <daniela.eberhard@pm.com>
2022-04-19 10:45:45 +0200

    import spring boot init folder

---
+++ wg-mate-backend/src/main/java/at/jku/wgmatebackend/WgMateBackendApplication.java
@@ -0,0 +1,13 @@
+package at.jku.wgmatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgMateBackendApplication.class, args);
+	}
+
+}
