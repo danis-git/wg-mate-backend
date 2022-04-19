f45e7007a96949516a929b383c1af9f9cb601ffd
Dani <daniela.eberhard@pm.com>
2022-04-19 13:29:41 +0200

    Yeah

--- src/main/java/at/jku/wgmatebackend/HelloController.java
+++ src/main/java/at/jku/wgmatebackend/HelloController.java
@@ -1,2 +1,14 @@
-package at.jku.wgmatebackend;public class HelloController {
+package at.jku.wgmatebackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
 }
