bca5f1f36c27fb025f0cf0f42cfa82feed39b74b
Sebastian <sebastian.woeber@gmail.com>
2022-04-19 15:03:29 +0200

    Mysql_connection

--- src/main/java/at/jku/wgmatebackend/WgMateBackendApplication.java
+++ src/main/java/at/jku/wgmatebackend/WgMateBackendApplication.java
@@ -1,14 +1,18 @@
 package at.jku.wgmatebackend;
 
+import org.hibernate.annotations.Entity;
+import org.hibernate.annotations.Table;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -33,5 +37,28 @@ public class WgMateBackendApplication {
 
 		};
 	}
+	@Entity
+	@Table(appliesTo = "name")
+	public class UserEntity {
 
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		private String firstName;
+		private String lastName;
+		@Column(unique = true)
+		private String email;
+		private String password;
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		@Id
+		public Long getId() {
+			return id;
+		}
+
+		//getter setters
+	}
 }
